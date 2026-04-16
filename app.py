9e78a032e1b3fd9791715c061747a0170ffa8600
Anil <bhardwaj3anil1@gamil.com>
2026-04-16 15:41:33 +0530

    fix nltk deployment

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords
 import nltk
+
+# Download required NLTK data first
+nltk.download('punkt', quiet=True)
+nltk.download('stopwords', quiet=True)
+
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
-nltk.download('punkt')
-nltk.download('stopwords')
+
 ps = PorterStemmer()
 
 
